f1a353b2c3f1fcd4fa967d9de4b9b48050647dda
Lilieyen <lnyasita@gmail.com>
2021-07-18 05:20:58 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """print out the pascal triangle"""
 
+
 def pascal_triangle(n):
     list1 = []
     if n <= 0:
         return list1
-    for i in range(n):
+    for i in range(1, n + 1):
         list2 = []
-    for j in range(i + 1):
-        if (j == 0) or (j == i) or (j < i):
-            list2.append(1)
-    else:
-        list2.append(list1[i - 1][j] + list1[i - 1][j - 1])
-    list1.append(list2)
+        C = 1
+        for j in range(1, i + 1):
+            list2.append(C)
+            C = C * (i - j) // j
+            list1.append(list2)
 
     return list1
