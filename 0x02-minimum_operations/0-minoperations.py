ebfea76da3bab3c328f4be02e416ac5acd97f827
Lilieyen <lnyasita@gmail.com>
2021-07-29 21:51:27 +0300

    min operations, copy and paste

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,6 +13,7 @@ def minOperations(n):
     if n == 1:
         return n - 1
 
+    """loop for n number of times"""
     for i in range(2, n + 1):
         if n % i == 0:
             return minOperations(int(n / i)) + i
